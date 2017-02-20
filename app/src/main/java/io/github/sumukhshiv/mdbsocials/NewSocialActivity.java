0b18d7a827722f28ecaf1b9d1ef988cd2012d8a5
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2017-02-20 11:12:56 -0800

    Dialogue Box

--- app/src/main/java/io/github/sumukhshiv/mdbsocials/NewSocialActivity.java
+++ app/src/main/java/io/github/sumukhshiv/mdbsocials/NewSocialActivity.java
@@ -3,6 +3,8 @@ package io.github.sumukhshiv.mdbsocials;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NewSocialActivity extends AppCompatActivity {
 
@@ -22,6 +25,7 @@ public class NewSocialActivity extends AppCompatActivity {
         Button buttonUploadPicture = (Button) findViewById(R.id.buttonUploadPicture);
         EditText editTextDate = (EditText) findViewById(R.id.editTextDate);
         EditText editTextDescription = (EditText) findViewById(R.id.editTextDescription);
+        ImageView mImageView = (ImageView) findViewById(R.id.mImageView);
 
         buttonUploadPicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +36,13 @@ public class NewSocialActivity extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 //Open Camera
+                                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                                    startActivityForResult(takePictureIntent, 1);
+                                }
+//                                Bundle extras = takePictureIntent.getExtras();
+//                                Bitmap imageBitmap = (Bitmap) extras.get("data");
+//                                mImageView.setImageBitmap(imageBitmap);
                                 dialog.dismiss();
                             }
                         });
